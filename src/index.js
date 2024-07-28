ed6d82a5891b9db07a08560c7085d7c15014962e
Masum Khan <masumkhan081@gmail.com>
2024-07-28 18:08:40 +0600

    to deploy

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
-// /* eslint-disable no-undef */
-// const app = require("./app");
-// const mongodbConnection = require("./config/mongodb");
-// const config = require("./config");
-// require("dotenv").config();
+/* eslint-disable no-undef */
+const app = require("./app");
+const mongodbConnection = require("./config/mongodb");
+const config = require("./config");
+require("dotenv").config();
 
-// async function bootstrap() {
-//   const server = app.listen(config.port, async () => {
-//     console.log(`Server running on port ${config.port}`);
-//     await mongodbConnection();
-//   });
+async function bootstrap() {
+  const server = app.listen(config.port, async () => {
+    console.log(`Server running on port ${config.port}`);
+    await mongodbConnection();
+  });
 
-//   app.get("/", (req, res) => {
-//     res.send("Welcome .. ");
-//   });
+  app.get("/", (req, res) => {
+    res.send("Welcome .. ");
+  });
 
-//   const exitHandler = () => {
-//     if (server) {
-//       server.close(() => {
-//         console.log("Server closed");
-//       });
-//     }
-//     process.exit(1);
-//   };
+  const exitHandler = () => {
+    if (server) {
+      server.close(() => {
+        console.log("Server closed");
+      });
+    }
+    process.exit(1);
+  };
 
-//   const unexpectedErrorHandler = (error) => {
-//     console.log(error);
-//     exitHandler();
-//   };
+  const unexpectedErrorHandler = (error) => {
+    console.log(error);
+    exitHandler();
+  };
 
-//   process.on("uncaughtException", unexpectedErrorHandler);
-//   process.on("unhandledRejection", unexpectedErrorHandler);
-// }
+  process.on("uncaughtException", unexpectedErrorHandler);
+  process.on("unhandledRejection", unexpectedErrorHandler);
+}
 
-// bootstrap();
+bootstrap();
