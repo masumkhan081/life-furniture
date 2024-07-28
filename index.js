61237f829a248476c886e994d35d448510f47064
Masum Khan <masumkhan081@gmail.com>
2024-07-28 17:55:49 +0600

    to deploy

---
+++ index.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-undef */
+const app = require("./src/app");
+const mongodbConnection = require("./config/mongodb");
+const config = require("./config");
+require("dotenv").config();
+
+async function bootstrap() {
+  const server = app.listen(config.port, async () => {
+    console.log(`Server running on port ${config.port}`);
+    await mongodbConnection();
+  });
+
+  app.get("/", (req, res) => {
+    res.send("Welcome .. ");
+  });
+
+  const exitHandler = () => {
+    if (server) {
+      server.close(() => {
+        console.log("Server closed");
+      });
+    }
+    process.exit(1);
+  };
+
+  const unexpectedErrorHandler = (error) => {
+    console.log(error);
+    exitHandler();
+  };
+
+  process.on("uncaughtException", unexpectedErrorHandler);
+  process.on("unhandledRejection", unexpectedErrorHandler);
+}
+
+bootstrap();
