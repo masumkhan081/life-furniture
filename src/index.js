3afa85a80fcecc1fce99dce47d9ca477557cef1e
Masum Khan <masumkhan081@gmail.com>
2024-07-28 18:11:11 +0600

    to deploy

--- src/index.js
+++ src/index.js
@@ -8,11 +8,7 @@ async function bootstrap() {
   const server = app.listen(config.port, async () => {
     console.log(`Server running on port ${config.port}`);
     await mongodbConnection();
-  });
-
-  app.get("/", (req, res) => {
-    res.send("Welcome .. ");
-  });
+  }); 
 
   const exitHandler = () => {
     if (server) {
